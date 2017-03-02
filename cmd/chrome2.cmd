e6f1f4330b8d54fd9012f2ddf8eb04d8ad675c4b
mosh <moshahmed@gmail.com>
2017-03-02 14:13:43 +0530

    new

--- cmd/chrome2.cmd
+++ cmd/chrome2.cmd
@@ -1,16 +1,42 @@
 @echo off
-:: Script to launch multiple independent copies of chromes on windows.
-:: GPL(C) 2017 moshahmed
-
+:: WHAT: Script to launch multiple chromes
 if [%1] == [] (
   @echo USAGE: CHROME2.CMD GMAILID CTMPDIR URL
+  @echo USAGE: CHROME2.CMD me                 .. default gmail
   goto :eof
 )
 
+:: Find CHROME_EXE
+@for %%D in (
+    C:\Progra~2\Google\Chrome\Application\chrome.exe
+    C:\Progra~1\Google\Chrome\Application\chrome.exe
+    C:\Progra~1\Google\Chrome\Applic~1\chrome.exe
+    C:\Users\%USERNAME%\AppData\Local\Google\Chrome\Application\chrome.exe
+  ) do (
+  @IF EXIST %%D (
+    @set CHROME_EXE=%%D
+    @echo CHROME_EXE=%CHROME_EXE%
+    @break
+  )
+)
+
+@IF not EXIST %CHROME_EXE% (
+  @echo NO CHROME_EXE=%CHROME_EXE%
+  goto :eof
+)
+
+:: Get the args
 set GMAILID=%1
 set CTMPDIR=%2
 set URL=%3
 
+:: Start default chrome
+if [%GMAILID%] == [me] (
+  @start %CHROME_EXE% %URL%
+  goto :eof
+)
+
+:: Setup new temp directory for GMAILID
 if [%CTMPDIR%] == [] (
   set CTMPDIR=%TEMP%
 )
@@ -27,18 +53,8 @@ set     CDATA=%CTMPDIR%\chrome2-data-%GMAILID%
 @echo   CDATA=%CDATA%
 @mkdir %CDATA%
 
-@rem Launch Chrome
-@for %%D in (
-    C:\Progra~2\Google\Chrome\Application\chrome.exe
-    C:\Progra~1\Google\Chrome\Application\chrome.exe
-    C:\Progra~1\Google\Chrome\Applic~1\chrome.exe
-    C:\Users\%USERNAME%\AppData\Local\Google\Chrome\Application\chrome.exe
-  ) do (
-  @IF EXIST %%D (
-    @echo Found CHROME_EXE=%%D
-    @echo CHROME_EXE --enable-udd-profiles /N --user-data-dir=%CDATA% %URL%
-    @start       %%D --enable-udd-profiles /N --user-data-dir=%CDATA% %URL%
-    @break
-  )
-  goto :eof
-)
+:: Launch Chrome
+@echo   CHROME_EXE  --enable-udd-profiles /N --user-data-dir=%CDATA% %URL%
+@start %CHROME_EXE% --enable-udd-profiles /N --user-data-dir=%CDATA% %URL%
+goto :eof
+
